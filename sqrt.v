c22d7219d1bfb9ad567f25489ccec6a1b5c117b2
Larry D. Lee Jr <llee454@gmail.com>
2020-05-16 21:04:23 -0400

    Proved several assertions: error_Sn, sqr_expand, b_0, b_1, le_eq, le_eq_comm, approx_sqr_is_positive, error_upper_bound_a, approx_upper_bound, and a_upper_bound_0.

--- sqrt.v
+++ sqrt.v
@@ -119,32 +119,82 @@ Proof
 *)
 Lemma error_Sn
   :  forall n : nat, error (S n) = error n - (b n)/(2^n) * (2 * approx n + (b n)/(2^n)).
-Proof
-  fun n =>
-    eq_sym (error_n (S n))
-    || error (S n) = a - X^2 @X by <- approx_Sn n
-    || _ = a - (approx n + b n/2^n) * X @X by <- Rmult_1_r (approx n + b n/2^n)
-    || _ = _ - X @X by <- Rmult_plus_distr_r (approx n) ((b n)/(2^n)) (approx n + b n/2^n)
-    || _ = _ - (X + _) @X by <- Rmult_plus_distr_l (approx n) (approx n) (b n/2^n)
-    || _ = _ - (_ + X) @X by <- Rmult_plus_distr_l (b n/2^n) (approx n) (b n/2^n)
-    || _ = _ - ((X + _) + _) @X by pow2 (approx n)
-    || _ = _ - X @X by Rplus_assoc ((approx n)^2 + approx n * (b n/2^n)) (b n/2^n * approx n) ((b n/2^n) * (b n/2^n))
-    || _ = _ - (X + _) @X by <- Rplus_assoc ((approx n)^2) (approx n * (b n/2^n)) (b n/2^n * approx n)
-    || _ = _ - (_ + (_ + X) + _) @X by <- Rmult_comm (b n/2^n) (approx n)
-    || _ = _ - (_ + X + _) @X by double (approx n * (b n/2^n))
-    || _ = _ - X @X by <- Rplus_assoc ((approx n)^2) (2 * ((approx n) * (b n/2^n))) ((b n/2^n)*(b n/2^n))
-    || _ = _ + X @X by <- Ropp_plus_distr ((approx n)^2) (2 * (approx n * (b n/2^n)) + (b n/2^n)*(b n/2^n))
-    || _ = X @X by Rplus_assoc a (- (approx n)^2) (- (2*(approx n * (b n/2^n)) + (b n/2^n)*(b n/2^n)))
-    || _ = X + _ @X by <- error_n n
-    || _ = _ - (X + _) @X by Rmult_assoc 2 (approx n) (b n/2^n)
-    || _ = _ - X @X by Rmult_plus_distr_r (2 * (approx n)) (b n/2^n) (b n/2^n)
-    || _ = _ - X @X by <- Rmult_comm (2 * approx n + b n/2^n) (b n/2^n).
+Proof.
+  exact
+    (fun n =>
+      eq_sym (error_n (S n))
+      || error (S n) = a - X^2 @X by <- approx_Sn n
+      || _ = a - (approx n + b n/2^n) * X @X by <- Rmult_1_r (approx n + b n/2^n)
+      || _ = _ - X @X by <- Rmult_plus_distr_r (approx n) ((b n)/(2^n)) (approx n + b n/2^n)
+      || _ = _ - (X + _) @X by <- Rmult_plus_distr_l (approx n) (approx n) (b n/2^n)
+      || _ = _ - (_ + X) @X by <- Rmult_plus_distr_l (b n/2^n) (approx n) (b n/2^n)
+      || _ = _ - ((X + _) + _) @X by pow2 (approx n)
+      || _ = _ - X @X by Rplus_assoc ((approx n)^2 + approx n * (b n/2^n)) (b n/2^n * approx n) ((b n/2^n) * (b n/2^n))
+      || _ = _ - (X + _) @X by <- Rplus_assoc ((approx n)^2) (approx n * (b n/2^n)) (b n/2^n * approx n)
+      || _ = _ - (_ + (_ + X) + _) @X by <- Rmult_comm (b n/2^n) (approx n)
+      || _ = _ - (_ + X + _) @X by double (approx n * (b n/2^n))
+      || _ = _ - X @X by <- Rplus_assoc ((approx n)^2) (2 * ((approx n) * (b n/2^n))) ((b n/2^n)*(b n/2^n))
+      || _ = _ + X @X by <- Ropp_plus_distr ((approx n)^2) (2 * (approx n * (b n/2^n)) + (b n/2^n)*(b n/2^n))
+      || _ = X @X by Rplus_assoc a (- (approx n)^2) (- (2*(approx n * (b n/2^n)) + (b n/2^n)*(b n/2^n)))
+      || _ = X + _ @X by <- error_n n
+      || _ = _ - (X + _) @X by Rmult_assoc 2 (approx n) (b n/2^n)
+      || _ = _ - X @X by Rmult_plus_distr_r (2 * (approx n)) (b n/2^n) (b n/2^n)
+      || _ = _ - X @X by <- Rmult_comm (2 * approx n + b n/2^n) (b n/2^n)).
+Qed.
+
+(*
+  In each iteration [n], we try to append a 1
+  bit onto [approx]. If the result is larger than
+  [a], we append a 0 instead.
+*)
+Axiom bn : forall n : nat, (approx n + 1/2^n)^2 > a <-> b n = 0.
+
+Lemma sqr_expand : forall a b : R, (a + b)^2 = a^2 + b * (2 * a + b).
+Proof.
+  exact
+    (fun a b =>
+      eq_refl ((a + b)^2)
+      || _ = (a + b) * X @X by <- Rmult_1_r (a + b)
+      || _ = X @X by <- Rmult_plus_distr_r a b (a + b)
+      || _ = X + _ @X by <- Rmult_plus_distr_l a a b
+      || _ = X + _ + _ @X by pow2 a
+      || _ = _ + _ + X @X by <- Rmult_plus_distr_l b a b
+      || _ = X @X by <- Rplus_assoc (a^2) (a * b) (b * a + b * b)
+      || _ = _ + X @X by Rplus_assoc (a * b) (b * a) (b * b)
+      || _ = _ + (_ + X + _) @X by Rmult_comm a b
+      || _ = _ + (X + _) @X by double (a * b)
+      || _ = _ + (X + _) @X by Rmult_assoc 2 a b
+      || _ = _ + X @X by Rmult_plus_distr_r (2 * a) b b
+      || _ = _ + X @X by <- Rmult_comm (2 * a + b) b).
+Qed.
 
 (*
   Asserts bounds for [error] and [approx] based
   on the value of a given bit.
+
+  a < (approx (n) + 1/2^n)^2
+  approx (n)^2 + error (n) < (apporx (n) + 1/2^n)^2
+  approx (n)^2 + error (n) < approx (n)^2 + 1/2^n (2 approx (n) + 1/2^n)
+  error (n) < approx (n)^2 + 1/2^n (2 approx (n) + 1/2^n)
 *)
-Axiom b_0 : forall n : nat, b n = 0 -> error n < 1/2^n * (2 * approx n + 1/2^n).
+Lemma b_0 : forall n : nat, b n = 0 -> error n < 1/2^n * (2 * approx n + 1/2^n).
+Proof.
+  exact
+    (fun n H =>
+      Rplus_lt_compat_l
+        (- (approx n)^2)
+        ((approx n)^2 + error n)
+        ((approx n)^2 + (1/2^n * (2 * approx n + 1/2^n)))
+        (Rgt_lt _ _ (proj2 (bn n) H)
+          || X < (approx n + 1/2^n)^2 @X by <- spec n
+          || _ < X @X by <- sqr_expand (approx n) (1/2^n))
+      || X < _ @X by Rplus_assoc (- (approx n)^2) ((approx n)^2) (error n)
+      || _ < X @X by Rplus_assoc (- (approx n)^2) ((approx n)^2) (1/2^n * (2 * approx n + 1/2^n))
+      || X + _ < _ @X by <- Rplus_opp_l ((approx n)^2)
+      || X < _ @X by <- Rplus_0_l (error n)
+      || _ < X + _ @X by <- Rplus_opp_l ((approx n)^2)
+      || _ < X @X by <- Rplus_0_l _).
+Qed.
 
 (*
   0 <= error (n + 1)
@@ -153,20 +203,22 @@ Axiom b_0 : forall n : nat, b n = 0 -> error n < 1/2^n * (2 * approx n + 1/2^n).
 *)
 Lemma b_1 : forall n : nat, b n = 1 -> 1/2^n * (2 * approx n + 1/2^n) <= error n.
 Proof.
-  fun n H =>
-    Rplus_le_compat_r
-      (b n/2^n * (2 * approx n + b n/2^n)) 0
-      (error n - b n/2^n * (2 * approx n + b n/2^n))
-      (error_is_positive (S n)
-       || 0 <= X @X by <- error_Sn n)
-    || X <= _ @X by <- Rplus_0_l (b n/2^n * (2 * approx n + b n/2^n))
-    || _ <= X @X by <-
-      Rplus_assoc (error n)
-        (- (b n/2^n * (2 * approx n + b n/2^n)))
-        (b n/2^n * (2 * approx n + b n/2^n))
-    || _ <= _ + X @X by <- Rplus_opp_l (b n / 2 ^ n * (2 * approx n + b n / 2 ^ n))
-    || _ <= X @X by <- Rplus_0_r (error n)
-    || X @X by ltac:(rewrite H; reflexivity).
+  exact
+    (fun n H =>
+      Rplus_le_compat_r
+        (b n/2^n * (2 * approx n + b n/2^n)) 0
+        (error n - b n/2^n * (2 * approx n + b n/2^n))
+        (error_is_positive (S n)
+         || 0 <= X @X by <- error_Sn n)
+      || X <= _ @X by <- Rplus_0_l (b n/2^n * (2 * approx n + b n/2^n))
+      || _ <= X @X by <-
+        Rplus_assoc (error n)
+          (- (b n/2^n * (2 * approx n + b n/2^n)))
+          (b n/2^n * (2 * approx n + b n/2^n))
+      || _ <= _ + X @X by <- Rplus_opp_l (b n / 2 ^ n * (2 * approx n + b n / 2 ^ n))
+      || _ <= X @X by <- Rplus_0_r (error n)
+      || X @X by ltac:(rewrite H; reflexivity)).
+Qed.
 
 (*
   Proves that every bit is greater than or equal
@@ -174,44 +226,48 @@ Proof.
 *)
 Lemma b_lower_bound
   :  forall n : nat, 0 <= b n.
-Proof
-  fun n
-    => sumbool_ind
-         (fun _ => 0 <= b n)
-         (fun H : b n = 0
-           => Req_le_sym 0 (b n) H)
-         (fun H : b n = 1
-           => Rle_0_1 || 0 <= X @X by H)
-         (b_is_bit n).
+Proof.
+  exact
+    (fun n
+      => sumbool_ind
+           (fun _ => 0 <= b n)
+           (fun H : b n = 0
+             => Req_le_sym 0 (b n) H)
+           (fun H : b n = 1
+             => Rle_0_1 || 0 <= X @X by H)
+           (b_is_bit n)).
+Qed.
 
 (**
   Proves that [approx] is always positive.
 *)
 Lemma approx_is_positive
   :  forall n : nat, 0 <= approx n.
-Proof
-  nat_ind _
-    (Req_le_sym 0 (approx 0) approx_0)
-    (fun n (H : 0 <= approx n)
-      => Rle_trans 0
-           (approx n + 0)
-           (approx n + (b n)/(2^n))
-           (H || 0 <= X @X by Rplus_0_r (approx n))
-           (Rplus_le_compat_l
-             (approx n)
-             0
-             ((b n)/(2^n))
-             ((Rmult_le_compat_r
-               (/(2^n))
+Proof.
+  exact
+    (nat_ind _
+      (Req_le_sym 0 (approx 0) approx_0)
+      (fun n (H : 0 <= approx n)
+        => Rle_trans 0
+             (approx n + 0)
+             (approx n + (b n)/(2^n))
+             (H || 0 <= X @X by Rplus_0_r (approx n))
+             (Rplus_le_compat_l
+               (approx n)
                0
-               (b n)
-               (Rlt_le 0 (/(2^n))
-                 (Rinv_0_lt_compat (2^n)
-                    (pow_lt 2 n Rlt_0_2)))
-               (b_lower_bound n))
-               || X <= (b n)/(2^n) @X by <- Rmult_0_l (/(2^n)))
-             )
-         || 0 <= X @X by (approx_Sn n)).
+               ((b n)/(2^n))
+               ((Rmult_le_compat_r
+                 (/(2^n))
+                 0
+                 (b n)
+                 (Rlt_le 0 (/(2^n))
+                   (Rinv_0_lt_compat (2^n)
+                      (pow_lt 2 n Rlt_0_2)))
+                 (b_lower_bound n))
+                 || X <= (b n)/(2^n) @X by <- Rmult_0_l (/(2^n)))
+               )
+           || 0 <= X @X by (approx_Sn n))).
+Qed.
 
 (**
   Expresses the square of [approx] in terms of
@@ -219,12 +275,14 @@ Proof
 *)
 Lemma approx_n_sqr
   :  forall n : nat, a - error n = (approx n)^2.
-Proof
-  fun n
-    => Rplus_eq_compat_r (- error n) a ((approx n)^2 + error n) (spec n)
-         || a - error n = X                @X by <- Rplus_assoc ((approx n)^2) (error n) (- (error n))
-         || a - error n = (approx n)^2 + X @X by <- Rplus_opp_r (error n)
-         || a - error n = X                @X by <- Rplus_0_r ((approx n)^2).
+Proof.
+  exact
+    (fun n
+      => Rplus_eq_compat_r (- error n) a ((approx n)^2 + error n) (spec n)
+           || a - error n = X                @X by <- Rplus_assoc ((approx n)^2) (error n) (- (error n))
+           || a - error n = (approx n)^2 + X @X by <- Rplus_opp_r (error n)
+           || a - error n = X                @X by <- Rplus_0_r ((approx n)^2)).
+Qed.
 
 (**
   Represents the [approx] in terms of [a] and
@@ -232,19 +290,51 @@ Proof
 *)
 Lemma approx_n
   :  forall n : nat, sqrt (a - error n) = approx n.
-Proof
-  fun n
-    => f_equal sqrt (approx_n_sqr n)
-         || sqrt (a - error n) = X @X by <- sqrt_pow2 (approx n) (approx_is_positive n).
+Proof.
+  exact
+    (fun n
+      => f_equal sqrt (approx_n_sqr n)
+           || sqrt (a - error n) = X @X by <- sqrt_pow2 (approx n) (approx_is_positive n)).
+Qed.
 
 (**
 *)
 Lemma approx_sqr_is_positive_alt
   :  forall n : nat, 0 <= a - error n.
-Proof
-  fun n
-    => pow_le (approx n) 2 (approx_is_positive n)
-         || 0 <= X @X by approx_n_sqr n.
+Proof.
+  exact
+    (fun n
+      => pow_le (approx n) 2 (approx_is_positive n)
+           || 0 <= X @X by approx_n_sqr n).
+Qed.
+
+Lemma le_eq : forall x y z : R, 0 <= x -> x + y = z -> y <= z.
+Proof.
+  intros x y z Hx H.
+  rewrite <- H.
+  rewrite <- (Rplus_0_l y) at 1.
+  apply (Rplus_le_compat_r y 0 x Hx).
+Qed.
+
+Lemma le_eq_comm : forall x y z : R, 0 <= y -> x + y = z -> x <= z.
+Proof.
+  intros x y z Hy H.
+  rewrite (Rplus_comm x y) in H.
+  exact (le_eq y x z Hy H).
+Qed.
+
+Lemma approx_sqr_is_positive : forall n : nat, 0 <= (approx n)^2.
+Proof.
+  intro n.
+  apply (pow_le (approx n) 2 (approx_is_positive n)).
+Qed.
+
+Lemma error_upper_bound_a : forall n : nat, error n <= a.
+Proof.
+  intro n.
+  apply (le_eq ((approx n)^2) (error n) a (approx_sqr_is_positive n)).
+  exact (eq_sym (spec n)).
+Qed.
 
 (**
   Asserts a weak constant upper bound for
@@ -252,12 +342,28 @@ Proof
 
   approx n = sqrt(a - error n) < sqrt(2)
 *)
-Conjecture approx_upper_bound
+Lemma approx_upper_bound
   :  forall n : nat, approx n < sqrt 2.
+Proof.
+  intro n.
+  rewrite <- (sqrt_square (approx n) (approx_is_positive n)).
+  rewrite <- (pow2 (approx n)).
+  apply (sqrt_lt_1_alt ((approx n)^2) 2).
+  split.
+  + exact (approx_sqr_is_positive n).
+  + refine (Rle_lt_trans ((approx n)^2) a 2 _ a_upper_bound).
+    apply (le_eq_comm ((approx n)^2) (error n) a (error_is_positive n) (eq_sym (spec n))).
+Qed.
 
-(* verified using Maxima *)
-Conjecture a_upper_bound_0
+Lemma a_upper_bound_0
   : forall n : nat, approx n + 1/2^n + 1/2^n = approx n + 2/2^n.
+Proof.
+  intro n.
+  rewrite (Rplus_assoc (approx n) (1/2^n) (1/2^n)).
+  unfold Rdiv.
+  rewrite <- (Rmult_plus_distr_r 1 1 (/2^n)).
+  reflexivity.
+Qed.
 
 (* verified using Maxima *)
 Conjecture a_upper_bound_1
